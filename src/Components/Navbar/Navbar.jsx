81c22f7856aebcc8b518447822905f797a18f59c
profesorjand <profesorjand@gmail.com>
2024-03-15 14:58:19 -0300

    Arreglar menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Styles from './navbar.module.css';
 
 export default function Navbar({ lenguage }) {
-  const [top, setTop] = useState();
-  const [bottom, setBottom] = useState();
-  const [view, setView] = useState(false);
+  // const [top, setTop] = useState();
+  // const [bottom, setBottom] = useState();
+  // const [view, setView] = useState(false);
   const About = {
     es: 'Acerca de mí',
     en: 'About',
@@ -22,53 +22,56 @@ export default function Navbar({ lenguage }) {
     en: 'Contact me',
   };
 
-  useEffect(() => {
-    test();
-  }, []);
-
-  function test() {
-    const element = document.getElementById('proyects');
-    const viewportHeight =
-      window.innerHeight || document.documentElement.clientHeight;
-    const rect = element.getBoundingClientRect();
-    setTop(element.getBoundingClientRect().top);
-    setBottom(element.getBoundingClientRect().bottom);
-    if (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    ) {
-      setView(true);
+  function openMenu() {
+    var x = document.getElementById('myLinks');
+    if (x.style.display === 'block') {
+      x.style.display = 'none';
     } else {
-      setView(false);
+      x.style.display = 'block';
     }
-    // //console.log(window.scrollY)
-    // if(window.scrollY >= 1500 ){
-    //   setView(true)
-    //  // console.log("listo")
-    // }else{
-    //   setView(false)
-    //  // console.log("sali")
-    // }
   }
 
-  window.addEventListener('scroll', test);
+  // useEffect(() => {
+  //   test();
+  // }, []);
+
+  // function test() {
+  //   const element = document.getElementById('proyects');
+  //   const viewportHeight =
+  //     window.innerHeight || document.documentElement.clientHeight;
+  //   const rect = element.getBoundingClientRect();
+  //   setTop(element.getBoundingClientRect().top);
+  //   setBottom(element.getBoundingClientRect().bottom);
+  //   if (
+  //     rect.top >= 0 &&
+  //     rect.left >= 0 &&
+  //     rect.bottom <=
+  //       (window.innerHeight || document.documentElement.clientHeight) &&
+  //     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  //   ) {
+  //     setView(true);
+  //   } else {
+  //     setView(false);
+  //   }
+  //   // //console.log(window.scrollY)
+  //   // if(window.scrollY >= 1500 ){
+  //   //   setView(true)
+  //   //  // console.log("listo")
+  //   // }else{
+  //   //   setView(false)
+  //   //  // console.log("sali")
+  //   // }
+  // }
+
+  //window.addEventListener('scroll', test);
 
   return (
-    <div>
-      <div
-        className={
-          view ? `${Styles.sidenav} ${Styles.highlight}` : `${Styles.sidenav}`
-        }
-      >
-        {/* <div className={styles.sidenavFixed}> */}
+    <div className={Styles.mobileContainer}>
+      <div className={Styles.topnav}>
         <a href="#aboutMe">{About[lenguage]}</a>
         <a href="#skills">{Skills[lenguage]}</a>
         <a href="#proyects">{Proyects[lenguage]}</a>
         <a href="#contact">{Contact[lenguage]}</a>
-        {/* </div> */}
       </div>
     </div>
   );
